9c81deb684dc98fd5a77a7e3e452fc9b6716fd2c
akshayUr04 <akshayur0404@gmail.com>
2023-04-24 01:21:38 +0530

    usecase test

--- pkg/usecase/user_test.go
+++ pkg/usecase/user_test.go
@@ -2,6 +2,9 @@ package usecase
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"reflect"
 	"testing"
 
 	"github.com/akshayur04/project-ecommerce/pkg/common/helperStruct"
@@ -9,7 +12,33 @@ import (
 	"github.com/akshayur04/project-ecommerce/pkg/repository/mockRepo"
 	"github.com/go-playground/assert/v2"
 	"github.com/golang/mock/gomock"
+	"golang.org/x/crypto/bcrypt"
 )
+
+type eqCreateUserParamsMatcher struct {
+	arg      helperStruct.UserReq
+	password string
+}
+
+func (e eqCreateUserParamsMatcher) Matches(x interface{}) bool {
+	arg, ok := x.(helperStruct.UserReq)
+	if !ok {
+		return false
+	}
+	if err := bcrypt.CompareHashAndPassword([]byte(arg.Password), []byte(e.password)); err != nil {
+		return false
+	}
+	e.arg.Password = arg.Password
+	return reflect.DeepEqual(e.arg, arg)
+}
+
+func (e eqCreateUserParamsMatcher) String() string {
+	return fmt.Sprintf("matches arg %v and password %v", e.arg, e.password)
+}
+
+func EqCreateUserParams(arg helperStruct.UserReq, password string) gomock.Matcher {
+	return eqCreateUserParamsMatcher{arg, password}
+}
 
 func TestUserSignup(t *testing.T) {
 	ctrl := gomock.NewController(t)
@@ -32,12 +61,15 @@ func TestUserSignup(t *testing.T) {
 				Password: "123456789",
 			},
 			buildStub: func(userRepo mockRepo.MockUserRepository) {
-				userRepo.EXPECT().UserSignUp(gomock.Any(), helperStruct.UserReq{
-					Name:     "akshay",
-					Email:    "akshay@gmail.com",
-					Mobile:   "9072001341",
-					Password: "123456789",
-				}).Times(1).
+				userRepo.EXPECT().UserSignUp(gomock.Any(),
+					EqCreateUserParams(helperStruct.UserReq{
+						Name:     "akshay",
+						Email:    "akshay@gmail.com",
+						Mobile:   "9072001341",
+						Password: "123456789",
+					},
+						"123456789")).
+					Times(1).
 					Return(response.UserData{
 						Id:     1,
 						Name:   "akshay",
@@ -52,6 +84,30 @@ func TestUserSignup(t *testing.T) {
 				Mobile: "9072001341",
 			},
 			expectedError: nil,
+		},
+		{
+			name: "alredy exits",
+			input: helperStruct.UserReq{
+				Name:     "akshay",
+				Email:    "akshay@gmail.com",
+				Mobile:   "9072001341",
+				Password: "123456789",
+			},
+			buildStub: func(userRepo mockRepo.MockUserRepository) {
+				userRepo.EXPECT().UserSignUp(gomock.Any(),
+					EqCreateUserParams(helperStruct.UserReq{
+						Name:     "akshay",
+						Email:    "akshay@gmail.com",
+						Mobile:   "9072001341",
+						Password: "123456789",
+					},
+						"123456789")).
+					Times(1).
+					Return(response.UserData{},
+						errors.New("user alredy exits"))
+			},
+			expectedOutput: response.UserData{},
+			expectedError:  errors.New("user alredy exits"),
 		},
 	}
 	for _, tt := range testData {
